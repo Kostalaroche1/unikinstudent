2db0449513a32900a021d8405e0c7998be55b0db
KOSTA gabriel <125087394+Kostalaroche1@users.noreply.github.com>
2025-11-21 15:28:53 +0100

    readind pdf using @react-pdf-viewer/core and  @react-pdf-viewer/default-layou

--- src/Component/HeavyComponent/PdfViewer.jsx
+++ src/Component/HeavyComponent/PdfViewer.jsx
@@ -9,34 +9,13 @@ import "@react-pdf-viewer/default-layout/lib/styles/index.css";
 export default function PdfViewer({ fileUrl }) {
     if (!fileUrl) return null;
 
-    const defaultLayoutPluginInstance = defaultLayoutPlugin({
-        renderToolbar: (Toolbar) => (
-            <Toolbar>
-                {(slots) => {
-                    const {
-                        CurrentPageInput,
-                        ZoomOut,
-                        ZoomIn,
-                        ZoomPopover,
-                    } = slots;
-
-                    return (
-                        <div style={{ display: "flex", gap: "10px", padding: "8px" }}>
-                            <CurrentPageInput />
-                            <ZoomOut />
-                            <ZoomIn />
-                            <ZoomPopover />
-                        </div>
-                    );
-                }}
-            </Toolbar>
-        ),
-    });
+    // No custom toolbar override in v3.12.0
+    const layoutPlugin = defaultLayoutPlugin();
 
     return (
         <div style={{ height: "90vh" }}>
             <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.11.174/build/pdf.worker.min.js">
-                <Viewer fileUrl={fileUrl} plugins={[defaultLayoutPluginInstance]} />
+                <Viewer fileUrl={fileUrl} plugins={[layoutPlugin]} />
             </Worker>
         </div>
     );
