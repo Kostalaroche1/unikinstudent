ff6bad2dd1c2b400068c5e3dec18dbbc7cd49b5d
KOSTA gabriel <125087394+Kostalaroche1@users.noreply.github.com>
2025-11-22 07:51:24 +0100

    add google analytic link

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,6 +4,7 @@ import SessionWrappers from "@/session/SessionWrappers";
 import { ContextProvide } from "@/Component/ContextComponent/UserAuth";
 import BootstrapInstallation from "@/Component/Bootstrap";
 import FooterBiblos from "@/Component/HeavyComponent/FooterBilblos";
+import Script from "next/script";
 
 export default function RootLayout({ children }) {
 
@@ -13,6 +14,23 @@ export default function RootLayout({ children }) {
       <SessionWrappers>
         <html lang="en">
           <BootstrapInstallation />
+          <head>
+            <Script id="first id"
+              async
+              src="https://www.googletagmanager.com/gtag/js?id=G-62NKTG0X9C"
+            />
+            <Script id="second id"
+              dangerouslySetInnerHTML={{
+                __html: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-62NKTG0X9C');
+      `,
+              }}
+            />
+          </head>
+
           <body >
             <div className="page-container">
               <main className="content">{children}</main>
